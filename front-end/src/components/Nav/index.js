4459697bcb6e4d6983c55f53911e7696d6425786
Raman Hira <ramanhira@gmail.com>
2025-10-23 19:59:48 +0530

    updated the NavBar

--- front-end/src/components/Nav/index.js
+++ front-end/src/components/Nav/index.js
@@ -11,14 +11,14 @@ const Nav = () => {
   const logoutAndRedirectToSignUp = () => {
     localStorage.clear();
     navigate('/signup');
-  }
+  };
 
   const getName = () => {
     if (auth) {
       return JSON.parse(auth).name;
     }
     return '';
-  }
+  };
 
   useEffect(() => {
     const auth = localStorage.getItem('user');
@@ -29,43 +29,45 @@ const Nav = () => {
   
   return (
     <nav>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/">Products</Link>
-        </li>
-        <li>
-          <Link to="/add">Add Product</Link>
-        </li>
-        <li>
-          <Link to="/update">Update PRoduct</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          {
-            auth ?
-              (<Link
-                to="/logout"
-                onClick={logoutAndRedirectToSignUp}
-              >Logout ({getName()})</Link>) :
-              (
-                <>
-                  <Link
-                    to="/signup"
-                  >Sign Up</Link>
-
-                  <Link
-                    to="/login"
-                  >Log in</Link>
-                </>
-                
-              )
-          }
-        </li>
-
-        
-      </ul>
+      {
+        auth ?
+          (
+            <ul className="nav-ul">
+              <li>
+                <Link to="/">Products</Link>
+              </li>
+              <li>
+                <Link to="/add">Add Product</Link>
+              </li>
+              <li>
+                <Link to="/update">Update PRoduct</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link
+                  to="/logout"
+                  onClick={logoutAndRedirectToSignUp}
+                >Logout ({getName()})</Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="nav-ul nav-right">
+              <li>
+                <Link
+                  to="/signup"
+                >Sign Up</Link>
+              </li>
+              <li>
+                <Link
+                  to="/login"
+                >Log in</Link>
+              </li>
+            </ul>
+          )  
+      }
+      
     </nav>
   );
 };
